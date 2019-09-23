c0f3ace1cc06411533bed6e33361b4e236a55a36
ankush.dwivedi <ankush.dwivedi@philips.com>
2019-09-23 22:07:44 +0530

    Second Commit

---
+++ StaticAnalysisGUI/MainWindow.xaml.cs
@@ -0,0 +1,78 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Net;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+using ParseReportResharper;
+using FinalDecisionGatingParameter;
+using FinalDecisionGatingParameter;
+
+namespace StaticAnalysisGUI
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+
+            //ServiceReference1.Service1Client analyser = new ServiceReference1.Service1Client();
+            string username, reponame, level;
+            username = User.Text;
+            reponame = Repo.Text;
+            level = Level.Text;
+            WebClient analyser = new WebClient();
+            string url =string.Format(
+                "http://localhost:60202/Service1.svc/StaticAnalyserTool/Absolute/{0}/{1}/{2}",
+                username,reponame,level);
+            //string output = analyser.StaticAnalyserToolControllerAbsolute(username, reponame, "Level4");
+            byte[] data = analyser.DownloadData(url);
+            
+            MessageBox.Show(Encoding.ASCII.GetString(data));
+
+
+        }
+
+        private void Btn2_Click(object sender, RoutedEventArgs e)
+        {
+            Scroller.Text = "Analyzing...";
+            string report = File.ReadAllText(@"C:\Users\320050487\Desktop\Reports\ToolErrorDuplicationReport.txt");
+            Scroller.Text = report;
+        }
+
+        private void Button_Click_1(object sender, RoutedEventArgs e)
+        {
+            Service1 obj=new Service1();
+            int issues = obj.ParseResharperErrorReport();
+            int dups = obj.ParseResharperErrorReport();
+            tb1.Text = dups.ToString();
+            tb2.Text = issues.ToString();
+        }
+
+        private void Button_Click_2(object sender, RoutedEventArgs e)
+        {
+           //FinalDecisionGatingParameter.FinalDecisionGatingParameter obj=new FinalDecisionGatingParameter.FinalDecisionGatingParameter();
+           // StaticAnalyserToolController.Service1 obj1=new StaticAnalyserToolController.Service1();
+           // obj1.StaticAnalyserToolController()
+           // obj.MakeFinalDecisionRelativeParameter(, )
+        }
+    }
+}
